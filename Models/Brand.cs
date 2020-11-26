5f1866764ea09a025aae7f755303a51459aefc56
Sachin Datey <sdatey@fmtconsultants.com>
2020-11-26 09:56:10 +0000

    Add project files.

---
+++ Models/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MVC_With_DevOps_CI_CD.Models
+{
+    public partial class Brand
+    {
+        public Brand()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MVC_With_DevOps_CI_CD.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
